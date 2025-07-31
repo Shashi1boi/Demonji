419446b67bb345a942557154aea1dc916e3a2ae8
Shashi1boi <143737884+Shashi1boi@users.noreply.github.com>
2025-07-31 13:44:42 +0530

    Update feeder.php

--- public/demons/feeder.php
+++ public/demons/feeder.php
@@ -23,27 +23,30 @@ if (!$parsedUrl || !isset($parsedUrl['scheme'], $parsedUrl['host'])) {
     exit;
 }
 
+$headers = [
+    "Cookie: mac={$mac}; stb_lang=en; timezone=GMT",
+    "User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3",
+    "Authorization: Bearer {$token}",
+    "Accept: */*",
+    "Referer: http://{$host}/stalker_portal/c/",
+    "Connection: Keep-Alive",
+    "Accept-Encoding: gzip"
+];
+
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $targetUrl);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-curl_setopt($ch, CURLOPT_HEADER, false);
-curl_setopt($ch, CURLOPT_HTTPHEADER, [
-    "User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3",
-    "Authorization: Bearer {$token}",
-    "X-User-Agent: Model: MAG250; Link: WiFi",
-    "Referer: http://{$host}/stalker_portal/c/",
-    "Cookie: timezone=GMT; stb_lang=en; mac={$mac}",
-    "Accept: */*",
-    "Host: {$host}",
-    "Connection: Keep-Alive",
-    "Accept-Encoding: gzip"
-]);
+curl_setopt($ch, CURLOPT_HEADER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_ENCODING, 'gzip');
 
 $response = curl_exec($ch);
 $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+$headerSize = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+$headersReceived = substr($response, 0, $headerSize);
+$body = substr($response, $headerSize);
 $error = curl_error($ch);
 curl_close($ch);
 
@@ -56,35 +59,29 @@ if ($response === false) {
 if ($httpCode >= 400) {
     // Retry with a new token
     $token = generate_token(true);
+    $headers[2] = "Authorization: Bearer {$token}";
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $targetUrl);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
     curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-    curl_setopt($ch, CURLOPT_HEADER, false);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, [
-        "User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3",
-        "Authorization: Bearer {$token}",
-        "X-User-Agent: Model: MAG250; Link: WiFi",
-        "Referer: http://{$host}/stalker_portal/c/",
-        "Cookie: timezone=GMT; stb_lang=en; mac={$mac}",
-        "Accept: */*",
-        "Host: {$host}",
-        "Connection: Keep-Alive",
-        "Accept-Encoding: gzip"
-    ]);
+    curl_setopt($ch, CURLOPT_HEADER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
     curl_setopt($ch, CURLOPT_ENCODING, 'gzip');
     $response = curl_exec($ch);
     $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    $headerSize = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+    $headersReceived = substr($response, 0, $headerSize);
+    $body = substr($response, $headerSize);
     $error = curl_error($ch);
     curl_close($ch);
 
     if ($response === false || $httpCode >= 400) {
         http_response_code($httpCode);
-        echo json_encode(['error' => "Server returned error: HTTP $httpCode", 'response' => $response, 'details' => $error]);
+        echo json_encode(['error' => "Server returned error: HTTP $httpCode", 'headers' => $headersReceived, 'response' => $body, 'details' => $error]);
         exit;
     }
 }
 
-echo $response;
+echo $body;
 ?>
